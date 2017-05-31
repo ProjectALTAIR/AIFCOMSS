504dda2f74422bfd12982ac3ad5c29af69453545
Justin Albert <jalbert@justinalbertmacbookpro.dyndns.cern.ch>
2017-05-31 07:26:45 -0700

    fix a degrees vs radians error

--- ALTAIR_entities_p5frame.js
+++ ALTAIR_entities_p5frame.js
@@ -1105,7 +1105,7 @@ function getScopeTrackingCommand() {
   // altitudes more than 40 deg above the horizon.  The formula
   // gives the elevation correction in minutes of arc.
 
-  var refraction = 1.02 / tan(el + (10.3 / (el + 5.11)));
+  var refraction = 1.02 / tan(radians(degrees(el) + (10.3 / (degrees(el) + 5.11))));
   el = el + (refraction * radians(60.) * (1. - exp((groundEle-ele)/7650.)));
 
   var azString = hex(round(az * 65536. / TWO_PI), 4);
